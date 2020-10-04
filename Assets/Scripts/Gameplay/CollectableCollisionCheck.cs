3f8b68b3baedc70b267903dfa775fd519a22fef3
seromyr <55792226+seromyr@users.noreply.github.com>
2020-10-04 17:26:18 -0300

    Collectibles Spawning Mechanics
    - Increased version to 0.06a
    - Categorized scripts
    - Created 3 lanes of spawning collectables
    - Added a few more items to collect

---
+++ Assets/Scripts/Gameplay/CollectableCollisionCheck.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableCollisionCheck : MonoBehaviour
+{
+    private bool _switch;
+    public bool Ready { get {return _switch; } }
+    private Collider _target;
+    public Collider Target { get { return _target; } }
+
+    private SphereCollider sc;
+    private int chance;
+    private string origincalTag;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectable") || other.CompareTag("GoodCollectable"))
+        {
+            sc = other.transform.GetComponent<SphereCollider>();
+            origincalTag = other.tag;
+
+            _switch = true;
+            _target = other;
+        }
+        else if (other.CompareTag("UnCollectable"))
+        {
+            other.tag = origincalTag;
+            sc.isTrigger = false;
+
+            _switch = true;
+            _target = other;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Collectable") || other.CompareTag("GoodCollectable"))
+        {
+            _switch = false;
+            _target = other;
+
+            // Create a chance that this object is non-interactable
+            chance = Random.Range(0, 11);
+            if (chance >= 6)
+            {
+                other.transform.GetComponentInChildren<SpriteRenderer>().sprite = null;
+                other.tag = "UnCollectable";
+                sc.isTrigger = true;
+                chance = 0;
+            }
+            
+        }
+    }
+}

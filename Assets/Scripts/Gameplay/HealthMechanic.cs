2ab17061d15f5455671e6186607c0c6dc364e1a1
Joshua Capstick <55792213+misterpilgrim@users.noreply.github.com>
2020-10-05 22:18:20 -0300

    Added Stuff
    - Health Bar
    - Readjusted UI
    - Speedometer working with wheel/hamster speed
    - Progress meter (does not move through code yet)

---
+++ Assets/Scripts/Gameplay/HealthMechanic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthMechanic : MonoBehaviour
+{
+    // player reference
+    private Player player;
+    
+    // health UI
+    private Image health;
+    // all the health state sprites
+    [SerializeField]
+    private List<Sprite> healthGraphics;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthGraphics = new List<Sprite>();
+        healthGraphics.AddRange(Resources.LoadAll<Sprite>("UIComponents/Health"));
+        
+        player = GameObject.Find("Player").GetComponent<Player>();
+        health = GameObject.Find("Health").GetComponent<Image>();
+        
+        health.sprite = healthGraphics[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (player.Health)
+        {
+            case 0: health.sprite = healthGraphics[0]; break;
+            case 1: health.sprite = healthGraphics[2]; break;
+            case 2: health.sprite = healthGraphics[1]; break;
+            default: health.sprite = healthGraphics[1]; break;
+        }
+    }
+
+}
